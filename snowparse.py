edb71cac14f232684b4c2172e76c767532d1121c
danny crasto <danwald79@gmail.com>
2019-07-01 19:58:09 +0400

    adds more branching for errors b64 decoding

--- snowparse.py
+++ snowparse.py
@@ -5,9 +5,11 @@ import base64
 import io
 import json
 import re
+import binascii
 from collections import Counter
 
 def add_padding(data):
+    data= data.strip()
     return (data + '='*(len(data)%4)) if (len(data) % 4) else data
 
 def get_start_index(string, sub_str='cx":"', fr=0):
@@ -15,6 +17,9 @@ def get_start_index(string, sub_str='cx":"', fr=0):
 
 def get_schema_match(string):
     match = re.search(r'iglu:com.dubizzle\\*/([-A-Za-z0-9_]*)\\*/jsonschema', string)
+    return match.group(1) if match else None
+def extract_data_str(string):
+    match = re.search(r'&cx=([-A-Za-z0-9+=]*)', string)
     return match.group(1) if match else None
 
 def parse():
@@ -34,7 +39,7 @@ def parse():
             if start_index != -1:
                 start_index += 5
                 end_index=get_start_index(py_str, sub_str='"', fr=start_index)
-                #print("sTART:", start_index,"End:", end_index)
+                print("sTART:", start_index,"End:", end_index)
                 sub_str=py_str[start_index:end_index]
                 #print("sub_str\n", sub_str)
                 byte_sub_str=base64.b64decode(add_padding(sub_str))
@@ -65,13 +70,34 @@ def parse():
                         counts.update({'good':1})
                     else:
                         counts.update({'bad':1})
-                        #print("No second schema found")
+                        print("No second schema found")
                         sys.stderr.write(line)
 
         except KeyError as k:
             counts.update({'key':1})
-            sys.stdout.write('\n{} Error[{}] parsing line {}'.format(type(e), e, current))
+            sys.stdout.write('\n{} Error[{}] parsing line {}'.format(type(k), k, current))
             sys.stderr.write(line)
+        except binascii.Error as b:
+            match = extract_data_str(py_str)
+            if match:
+                try:
+                    byte_sub_str=base64.b64decode(add_padding(match))
+                except binascii.Error as b2:
+                    counts.update({'binasccii2':1})
+                    sys.stdout.write('\n{} Error[{}] parsing line {}'.format(type(b), b, current))
+                else:
+                    py_sub_str=str(byte_sub_str, 'utf-8', 'ignore')
+                    match = get_schema_match(py_sub_str)
+                    if match:
+                        schemas.update({match:1})
+                        counts.update({'good':1})
+                    else:
+                        counts.update({'bad2':1})
+                        sys.stderr.write(line)
+            else:
+                counts.update({'binasccii':1})
+                sys.stdout.write('\n{} Error[{}] parsing line {}'.format(type(b), b, current))
+                sys.stderr.write(line)
         except Exception as e:
             counts.update({'bad':1})
             sys.stdout.write('\n{} Error[{}] parsing line {}'.format(type(e), e,current))
